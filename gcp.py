c513a2b3d5c9c37e31147661f46bcaaeb31077c3
Gel0bmstu <leshikne@gmail.com>
2020-04-05 21:06:23 +0300

    # some lil fixes

--- gcp.py
+++ gcp.py
@@ -4,18 +4,20 @@ import sys
 import os
 
 if __name__ == "__main__":
-	args = sys.argv[1:]
+	args = sys.argv[1:]        
 
 	if len(args) == 0:
-		os.system(u'git add . && git commit -m \"# some lil fixes\" && git push origin master')
+		out = os.popen(u'git add . && git commit -m \"# some lil fixes\" && git push origin master').read()
 	elif len(args) ==1:
 		if os.path.exists(args[0]):
-			os.system(u'git add ' + args[0] + ' && git commit -m "# some lil fixes" && git push origin master')
+			out = os.popen(u'git add ' + args[0] + ' && git commit -m "# some lil fixes" && git push origin master').read()
 		else:
-			os.system(u'git add . && git commit -m "' + args[0] + '" && git push origin master')			
+			out = os.system(u'git add . && git commit -m "' + args[0] + '" && git push origin master').read()			
 	elif len(args) == 2:
-		os.system(u'git add '+ args[0] + ' && git commit -m "' + args[1] + '" && git push origin master')
+		out = os.popen(u'git add '+ args[0] + ' && git commit -m "' + args[1] + '" && git push origin master').read()
 	elif len(args) == 3:
-		os.system(u'git add '+ args[0] + ' && git commit -m "' + args[1] + '" && git push origin ' + args[2])
+		out = os.popen(u'git add '+ args[0] + ' && git commit -m "' + args[1] + '" && git push origin ' + args[2]).read()
 	else:
 		print(u'Allowed only 3 args')
+
+	# if out == ""
