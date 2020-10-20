b180ba58cf3246857a9a463321dabdb0a48e99f5
Gel0bmstu <leshikne@gmail.com>
2020-10-20 15:14:28 +0300

    update

--- update_notes.sh
+++ update_notes.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-joplin sync &&
+joplin-cc --profile ~/.config/joplin-desktop/ sync &&
 
-pushd $NOTES_DIR
-rm -rf ./raw/*
---log-level debug export --format raw "$NOTES_DIR/raw"
+pushd /home/gel0/notes
+rm -rf ./raw
+joplin-cc --profile ~/.config/joplin-desktop/ \
+  --log-level debug export --format md "./raw" &&
+
+sudo sqlite3 ~/.config/joplin-desktop/database.sqlite \
+  "UPDATE version SET version = 33;"
+
+git add . &&
+git commit -m "update" &&
+git push &&
 popd
-git add .
-git commit -m "update"
