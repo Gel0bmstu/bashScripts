e876ca4f045d2a04df0d4d52abee936b0a33731f
Gel0bmstu <leshikne@gmail.com>
2021-03-30 19:12:33 +0300

    # update

--- vm_ssh_enter.sh
+++ vm_ssh_enter.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-ip=$(virsh net-dhcp-leases default | grep -oh -e "192.168.122.[1-9]*" | dmenu "$@");
-
-echo "ip is: $ip";
+ip=$(virsh net-dhcp-leases default | grep -oh -e "192.168.122.[0-9]*" | dmenu "$@");
 
 [[ -n $ip ]] || exit;
 
-ssh-keygen -R $ip && ssh -o "StrictHostKeyChecking no" root@$ip;
+echo "ip is: $ip";
+
+ssh-keygen -R $ip && sshpass -p 123 ssh -o "StrictHostKeyChecking no" root@$ip;
+
+if [ $? != 0 ]; then
+        ssh-keygen -R $ip && sshpass -p 1q2w3e4r%T^Y ssh -o "StrictHostKeyChecking no" root@$ip;
+fi
